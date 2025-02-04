73a17593b10732cdc970e2e92bd4197441f630a0
amir <amryamiir@gmail.com>
2025-02-04 20:31:21 +0330

    [ADD] test page

--- hello-typescript/src/main.ts
+++ hello-typescript/src/main.ts
@@ -1,11 +1,36 @@
-import reverseArray from './Reversing'
+import {reverseArray} from './Reversing'
+import {numberType} from './number-type'
+import {sortArray} from './sort-array'
 
-import NumberType from './number-type'
+import {Entity} from "./entity-array";
 
-(function(){
-    reverseArray(['html' , 'css' , 'javascript' , 'typescript' , 'react'])
-}());
+function testReverseArray(){
+    const items = ['React' , 'next' , '3' , 'html']
+    console.log(reverseArray(items))
+}
 
-(function (){
-    NumberType(['react' , 2 ,'js', 5 ,'ts' , true , 6])
-}())
+function testNumberType(){
+    const items =['React' , 'next' , 3 , 'html' , 15 , 12]
+    console.log(numberType(items))
+}
+
+function testSortArray(){
+    const items : Entity[] = [
+        { createdAt: "2025-02-02T00:00:00.000Z" },
+        {
+            createdAt: "2025-01-31T00:00:00.000Z",
+            modifiedAt: "2025-02-03T00:00:00.000Z",
+            name: "amir",
+        },
+    ];
+
+    console.log(sortArray(items))
+}
+
+function main(){
+    testReverseArray()
+    testNumberType()
+    testSortArray()
+}
+
+main()
