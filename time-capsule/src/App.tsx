75e256a6996c4f9393b99754b24c531ea5fd4ad6
amir <amryamiir@gmail.com>
2025-02-12 05:58:57 +0330

    [ADD] raw and half html

--- time-capsule/src/App.tsx
+++ time-capsule/src/App.tsx
@@ -1,8 +1,46 @@
 import './App.css'
+import {Capsule} from "./types/capsule-type.ts";
 
 function App() {
+    const capsule : Capsule[] = [
+        {
+            id : 1,
+            title : 'gym',
+            describtion : 'going to the gym',
+            date  : new Date(2025 , 4 , 15),
+            vibe : 'good'
+        }
+    ]
   return (
-    <h1>hello Amir!</h1>
+      <>
+        <header>
+          <h1>Time Capsule</h1>
+        </header>
+        <main>
+          <div className='toolbar'>
+            <input type="text"/>
+
+            <select>
+              <option selected value="all">all</option>
+              <option value="work">work</option>
+              <option value="frindly">frindly</option>
+              <option value="family">family</option>
+            </select>
+
+            <button>..</button>
+
+          </div>
+            <div className="result">
+                <ul>
+                    {
+                        capsule.map((r)=> (
+                            <li>{r.title}</li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </main>
+      </>
   )
 }
 
---
+++ time-capsule/src/types/capsule-type.ts
@@ -0,0 +1,9 @@
+import {Vibe} from "./vibe.ts";
+
+export type Capsule = {
+    id : string | number,
+    title : string,
+    date : Date,
+    describtion : string,
+    vibe : Vibe
+}
