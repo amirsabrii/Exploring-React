96507c9bc0dd8def0e85868c642891554e2bb819
amir <amryamiir@gmail.com>
2025-02-14 18:44:56 +0330

    [ADD] text-input component , text-input styles

---
+++ time-capsule/src/components/Text-input/Text-input.tsx
@@ -0,0 +1,18 @@
+import { ComponentProps, ReactNode } from "react";
+
+import styles from "./Text-input.module.css";
+
+type Props = ComponentProps<"input"> & {
+  suffixIcon: ReactNode;
+};
+
+function TextInput({ suffixIcon, ...otherProps }: Props): ReactNode {
+  return (
+    <div className={styles["text-input"]}>
+      <input type="text" {...otherProps} />
+      <div className={styles.suffix}>{suffixIcon}</div>
+    </div>
+  );
+}
+
+export default TextInput;
--- time-capsule/src/components/Toolbar/Toolbar.tsx
+++ time-capsule/src/components/Toolbar/Toolbar.tsx
@@ -1,24 +1,29 @@
-import {ReactNode} from 'react'
+import { ReactNode } from "react";
 
-import styles from './Toolbar.module.css'
+import styles from "./Toolbar.module.css";
+import TextInput from "../Text-input/Text-input.tsx";
+import MingcuteSearch2Line from "../../icons/MingcuteSearch2Line.tsx";
 
-function Toolbar() : ReactNode{
-    return (
-      <div className={styles.toolbar}>
-        <input type="text" />
+function Toolbar(): ReactNode {
+  return (
+    <div className={styles.toolbar}>
+      <TextInput
+        placeholder="Search Note..."
+        suffixIcon={<MingcuteSearch2Line />}
+      />
 
-        <select>
-          <option selected value="all">
-            all
-          </option>
-          <option value="work">work</option>
-          <option value="frindly">frindly</option>
-          <option value="family">family</option>
-        </select>
+      <select>
+        <option selected value="all">
+          all
+        </option>
+        <option value="work">work</option>
+        <option value="frindly">frindly</option>
+        <option value="family">family</option>
+      </select>
 
-        <button>..</button>
-      </div>
-    );
+      <button>..</button>
+    </div>
+  );
 }
 
 export default Toolbar;
