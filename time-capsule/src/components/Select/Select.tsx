9522b0244b8317b3288ab836b31c3ff2ee4914cc
amir <amryamiir@gmail.com>
2025-02-18 22:47:54 +0330

    [ADD] Select component styles

--- time-capsule/src/components/Select/Select.tsx
+++ time-capsule/src/components/Select/Select.tsx
@@ -3,19 +3,30 @@ import { ComponentProps, ReactNode } from "react";
 import styles from "./Select.module.css";
 import { SelectOption } from "../../types/select-option.ts";
 
+import { clsx } from "clsx";
+
+type Variant = "solid" | "outlined";
+
 type Props = ComponentProps<"select"> & {
+  variant?: Variant;
   options: SelectOption[];
 };
 
-function Select({ options }: Props): ReactNode {
+function Select({
+  options,
+  variant = "solid",
+  ...otherProps
+}: Props): ReactNode {
   return (
-    <select className={styles.select}>
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      ))}
-    </select>
+    <div className={clsx(styles["select"], styles[variant])}>
+      <select {...otherProps}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 }
 
