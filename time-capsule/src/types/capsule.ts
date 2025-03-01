0a0c6be0183c95aee1e9165424f0cd492a379339
amir <amryamiir@gmail.com>
2025-03-01 21:51:51 +0330

    [UPDATE] types

--- time-capsule/src/types/capsule.ts
+++ time-capsule/src/types/capsule.ts
@@ -1,9 +1,9 @@
-import { Vibe } from "./vibe.ts";
+import { Category } from "./category.ts";
 
 export type Capsule = {
   id: string | number;
   title: string;
   date: Date;
   describtion: string;
-  vibe: Vibe;
+  category: Category;
 };
