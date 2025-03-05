f428150948b4a66321ba522124a5e8697be52295
amir <amryamiir@gmail.com>
2025-03-05 21:44:10 +0330

    [ADD] LocalStroge for Capsule State

--- time-capsule/src/Providers/CapsuleProviders.tsx
+++ time-capsule/src/Providers/CapsuleProviders.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, ReactNode, useState } from "react";
+import { PropsWithChildren, ReactNode, useEffect, useState } from "react";
 
 import { Capsule } from "../types/capsule.ts";
 import { CapsuleContext } from "../context/CapsuleContext.tsx";
@@ -6,13 +6,33 @@ import { CapsuleContext } from "../context/CapsuleContext.tsx";
 type Props = PropsWithChildren;
 
 function CapsuleProviders({ children }: Props): ReactNode {
-  const [capsule, setCpsule] = useState<Capsule[]>([]);
+  const [capsule, setCpsule] = useState<Capsule[]>(loadCapsuleInitialState);
+
+  useEffect(() => {
+    localStorage.setItem("capsules", JSON.stringify(capsule));
+  }, [capsule]);
 
   return (
     <CapsuleContext.Provider value={{ capsule, setCpsule }}>
       {children}
     </CapsuleContext.Provider>
   );
+}
+
+function loadCapsuleInitialState(): Capsule[] {
+  type LocalStorgeCapsule = Omit<Capsule, "date"> & { date: string };
+
+  const items = localStorage.getItem("capsules");
+
+  if (!items) {
+    return [];
+  }
+  const parsedCpaule = JSON.parse(items) as LocalStorgeCapsule[];
+
+  return parsedCpaule.map((capsule) => ({
+    ...capsule,
+    date: new Date(capsule.date),
+  }));
 }
 
 export default CapsuleProviders;
