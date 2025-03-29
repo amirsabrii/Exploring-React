2cf4a26374e6fe840000ae70a91c1b7b843eef44
amir <amryamiir@gmail.com>
2025-03-29 14:09:00 +0330

    [ADD] functioality carousel

--- where-Igo/src/pages/AttractionDetails/components/CaouselThumbnail/CaouselThumbnail.tsx
+++ where-Igo/src/pages/AttractionDetails/components/CaouselThumbnail/CaouselThumbnail.tsx
@@ -1,21 +1,50 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 import { Attraction } from "../../../../type/attraction.ts";
 
 import styles from "./CaouselThumbnail.module.css";
+import clsx from "clsx";
 
 type Props = {
   attraction: Attraction;
 };
 
 function CaouselThumbnail({ attraction }: Props): ReactNode {
+  const [indexSlide, setIndexSlide] = useState<number>(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndexSlide((old) => (old + 1) % attraction.carousel.length);
+    }, 4000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [attraction]);
+
   return (
-    <div className={styles.carousel}>
-      <img
-        src={`${import.meta.env.VITE_CDN_BASE_URL}/${attraction.thumbnail}`}
-        alt=""
-      />
-    </div>
+      <div className={styles.carousel}>
+          <ul style={{transform: `translateX(${indexSlide * 100}%)`}}>
+              {attraction.carousel.map((image) => (
+                  <li>
+                      <img src={`${import.meta.env.VITE_CDN_BASE_URL}/${image}`} alt=""/>
+                  </li>
+              ))}
+          </ul>
+
+          <div className={styles.bullets}>
+              {attraction.carousel.map((_, index) => (
+                  <button
+                      key={index}
+                      className={clsx(
+                          styles.bullet,
+                          index === indexSlide && styles.active,
+                      )}
+                      onClick={() => setIndexSlide(index)}
+                  />
+              ))}
+          </div>
+      </div>
   );
 }
 
