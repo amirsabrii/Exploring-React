055dc6fe0edc46a162697628e1f7a86ab8f28a07
amir <amryamiir@gmail.com>
2025-04-16 21:53:09 +0330

    [ADD] responcive to header

--- where-Igo/src/components/Header/Header.tsx
+++ where-Igo/src/components/Header/Header.tsx
@@ -1,46 +1,69 @@
-import {ReactNode, useContext} from "react";
+import { ReactNode, useContext, useState } from "react";
 
-import {Link, NavLink} from "react-router";
+import { Link, NavLink } from "react-router";
 
-import kojaberam from '../../icons/kojaberam.jpeg'
+import kojaberam from "../../icons/kojaberam.jpeg";
 
 import styles from "./Header.module.css";
 import clsx from "clsx";
 import Button from "../Button/Button.tsx";
-import {ThemeContext} from "../../context/ThemeContext.ts";
+import { ThemeContext } from "../../context/ThemeContext.ts";
 import MingcuteSunFill from "../../icons/MingcuteSunFill.tsx";
 import SolarMoonBold from "../../icons/SolarMoonBold.tsx";
+import MingcuteMenuFill from "../../icons/MingcuteMenuFill.tsx";
 
-type NavItem  = {
-    title : string,
-    href : string
-}
+type NavItem = {
+  title: string;
+  href: string;
+};
 
-const navItem : NavItem[] =  [
-    {title : 'خانه' , href : '/'},
-    {title : 'درباره ما' , href : '/about'},
-    {title : 'تماس با ما' , href : '/contact'},
-    {title : 'راهنمای سفر' , href : '/ravel-Guide'}
-]
+const navItem: NavItem[] = [
+  { title: "خانه", href: "/" },
+  { title: "درباره ما", href: "/about" },
+  { title: "تماس با ما", href: "/contact" },
+  { title: "راهنمای سفر", href: "/ravel-Guide" },
+];
 
 function Header(): ReactNode {
-    const {theme ,toggleTheme} = useContext(ThemeContext)
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  const [isopen, setIsopen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsopen(!isopen);
+  };
   return (
     <div className={styles["header"]}>
-      <Link to={'/'}  className={styles.logo}><img src={kojaberam} /></Link>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <MingcuteMenuFill />
+      </div>
 
-        <nav>
-            <ul>
-            <Button onClick={() => toggleTheme()} >{theme === "dark" ? <MingcuteSunFill/> : <SolarMoonBold/>}</Button>
-                {navItem.map(item => (
-                    <li key={item.title}>
-                        <NavLink to={item.href} className={({isActive}) => clsx(isActive && styles.active) }  > {item.title} </NavLink>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+      <nav>
+        <ul className={clsx(styles.menu, isopen && styles.active)}>
+          <Button
+            className={styles["theme-button"]}
+            onClick={() => toggleTheme()}
+            shape={"circle"}
+          >
+            {theme === "dark" ? <MingcuteSunFill /> : <SolarMoonBold />}
+          </Button>
+
+          {navItem.map((item) => (
+            <li key={item.title}>
+              <NavLink
+                to={item.href}
+                className={({ isActive }) => clsx(isActive && styles.active)}
+              >
+                {item.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <Link to={"/"} className={styles.logo}>
+        <img src={kojaberam} />
+      </Link>
     </div>
-  )
+  );
 }
 
 export default Header;
